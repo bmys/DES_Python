472f01dc01b1b4b1a3a7380d3f22063268b9e61b
arch <aa>
2019-04-12 00:01:06 +0200

    Initial commit

---
+++ des.py
@@ -0,0 +1,128 @@
+import tables
+
+
+def ascii_to_bytes_arr(text):
+    a = [bin(ord(x)).lstrip('0b') for x in text]
+    b = []
+
+    for byte in a:
+        bits_to_add = 8 - len(byte)
+        b.append('0' * bits_to_add + byte)
+    return b
+
+
+def bytes_to_bits(arr):
+    res = []
+    for byte in arr:
+        for bit in byte:
+            res.append(0) if bit == '0' else res.append(1)
+    return res
+
+
+def bit_arr_to_str(arr):
+    return ''.join([str(x) for x in arr])
+
+
+def text_to_arr(arr):
+    return [int(x) for x in arr]
+
+
+def arr_to_int(arr):
+    k = 0
+    counter = 1
+    arr.reverse()
+    for x in arr:
+        k += x * counter
+        counter *= 2
+    return k
+
+
+def rearrange(arr, idx_arr):
+    res = []
+    for idx in idx_arr:
+        res.append(arr[idx-1])
+    return res
+
+
+def shift_bits(bits, num):
+    beg = bits[:num]
+    return bits[num:] + beg
+
+
+def generate_sub_key(key, num):
+    l_side, r_side = split_half(key)
+
+    shift = tables.keyShiftTable[num]
+    l_side, r_side = shift_bits(l_side, shift), shift_bits(r_side, shift)
+    r = l_side + r_side
+    return r
+
+
+def split_half(arr):
+    mid = len(arr) // 2
+    return arr[:mid], arr[mid:]
+
+
+def xor_arr(arr1, arr2):
+    return [x ^ y for x, y in zip(arr1, arr2)]
+
+
+def get_s_box(arr, s_box_num):
+    s_box = tables.sBlocks[s_box_num]
+
+    row = [arr[0], arr[-1]]
+    row = arr_to_int(row)
+
+    col = arr[1:-1]
+    col = arr_to_int(col)
+    res = s_box[row][col]
+
+    res = [int(x) for x in list('{0:0b}'.format(res))]
+
+    padding = 4 - len(res)
+    for x in range(padding):
+        res.insert(0, 0)
+
+    return res
+
+
+def split_arr_to_chunks(arr, n):
+    new_arr = []
+    for i in range(0, len(arr), n):
+        new_arr.append(arr[i:i + n])
+    return new_arr
+
+
+def s_box_substitution(arr):
+    spl_arr = split_arr_to_chunks(arr, 6)
+    res = []
+    for idx, s_box in enumerate(spl_arr):
+        val = get_s_box(s_box, idx)
+        res.extend(val)
+    return res
+
+
+def p_box_substitution(arr):
+    return rearrange(arr, tables.pBlockPermutationTable)
+
+
+def final_permutation(arr):
+    return rearrange(arr, tables.finalPermutationTable)
+
+
+def encrypt(arr, key, encrypt=True):
+    ip = rearrange(arr, tables.initialPermutationTable)
+    key = rearrange(key, tables.keyPermutationTable)
+
+    l_block, r_block = split_half(ip)
+    new_left = r_block
+
+    sub_key = generate_sub_key(key, 0)
+
+    r_block = rearrange(r_block, tables.extensionPermutationTable)
+    r_block = xor_arr(r_block, sub_key)
+    r_block = s_box_substitution(r_block)
+    r_block = rearrange(r_block, tables.pBlockPermutationTable)
+    r_block = xor_arr(r_block, l_block)
+
+    return new_left + r_block
---
+++ test.py
@@ -0,0 +1,154 @@
+import unittest
+
+import des
+import tables
+
+
+class TestByteOperations(unittest.TestCase):
+
+    def test_text_to_bytes_arr(self):
+        self.assertEqual(des.ascii_to_bytes_arr('hello'), ['01101000', '01100101', '01101100', '01101100', '01101111'])
+
+    def test_bytes_to_bits(self):
+        self.assertEqual(
+            des.bytes_to_bits(des.ascii_to_bytes_arr('hello')),
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0,
+             1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1])
+
+    def test_bit_arr_to_str(self):
+        self.assertEqual(
+            des.bit_arr_to_str([0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0,
+                                1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1]),
+            '0110100001100101011011000110110001101111')
+
+    def test_rearrange(self):
+        arr1 = [1, 2, 3, 4, 5]
+        arr2 = [0, 1, 0, 1, 1]
+
+        idx_arr = [5, 4, 3, 2, 1]
+        idx_arr2 = [1, 2, 2, 2, 3]
+
+        self.assertEqual(des.rearrange(arr1, idx_arr), idx_arr)
+        self.assertEqual(des.rearrange(arr1, idx_arr2), [1, 2, 2, 2, 3])
+        self.assertEqual(des.rearrange(arr2, idx_arr2), [0, 1, 1, 1, 0])
+
+    def test_shift_bits(self):
+        arr1 = [1, 2, 3, 4, 5]
+        res = des.shift_bits(arr1, 1)
+
+        self.assertEqual(res, [2, 3, 4, 5, 1])
+
+        res = des.shift_bits(arr1, 2)
+        self.assertEqual(res, [3, 4, 5, 1, 2])
+        # self.assertEqual()
+
+    def test_arr_to_int(self):
+        arr1 = [0, 1, 0, 1]
+        res = des.arr_to_int(arr1)
+        self.assertEqual(res, 5)
+
+        arr1 = [1, 1, 0, 1]
+        res = des.arr_to_int(arr1)
+        self.assertEqual(res, 13)
+
+        # self.assertEqual()
+
+class TestPermutation(unittest.TestCase):
+
+    def test_initial_permutation(self):
+        plain = ['0', '0', '1', '0', '1', '0', '0', '1', '1', '1', '0', '1', '1', '0', '0', '0', '0', '0', '1', '0',
+                 '0',
+                 '0', '1', '1', '0', '0', '1', '1', '0', '1', '1', '1', '0', '0', '1', '1', '1', '1', '0', '0', '0',
+                 '1',
+                 '0', '1', '0', '1', '1', '0', '0', '1', '0', '0', '1', '0', '0', '1', '1', '1', '1', '1', '1', '0',
+                 '1', '1']
+        result = des.rearrange(plain, tables.initialPermutationTable)
+        result = ''.join(result)
+        self.assertEqual(result, '1110001010111010001110001100110110000010100111011101001110101100')
+
+    def test_initial_key_permutation(self):
+        key = ['1', '1', '0', '1', '0', '0', '0', '1', '1', '1', '0', '1', '0', '1', '0', '0', '1', '0', '1', '0', '0',
+               '0', '1', '1', '0', '1', '0', '1', '1', '1', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0',
+               '0', '1', '0', '0', '0', '1', '1', '1', '0', '1', '0', '1', '0', '0', '1', '0', '1', '0', '1', '0', '1',
+               '0']
+        result = des.rearrange(key, tables.keyPermutationTable)
+        # print(result)
+        result = ''.join(result)
+        self.assertEqual(result, '11010111010010111001010001101001010001001010100110001011')
+
+    # def test_shift_key(self):
+    #     key = ['1', '1', '0', '1', '0', '1', '1', '1', '0', '1', '0', '0', '1', '0', '1', '1', '1', '0', '0', '1', '0', '1', '0', '0', '0', '1', '1', '0', '1', '0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '1', '0', '1', '0', '1', '0', '0', '1', '1', '0', '0', '0', '1', '0', '1', '1']
+    #
+    #     result = des.shift_bits(key, 1)
+    #     result = ''.join(result)
+    #     self.assertEqual(result, '10101110100101110010100011010010100010010101001100010111')
+
+    def test_generate_sub_key(self):
+        key = des.text_to_arr('11010111010010111001010001101001010001001010100110001011')
+
+        result = des.generate_sub_key(key, 0)
+        result = des.bit_arr_to_str(result)
+        self.assertEqual(result, '10101110100101110010100011010010100010010101001100010111')
+
+    def test_compression_key(self):
+        sub_key = des.text_to_arr('10101110100101110010100011010010100010010101001100010111')
+
+        result = des.rearrange(sub_key, tables.compressionPermutationTable)
+        result = des.bit_arr_to_str(result)
+        self.assertEqual(result, '100011111110011010110000011111010011100100010000')
+
+    def test_expansion_block(self):
+        sub_key = des.text_to_arr('10000010100111011101001110101100')
+
+        result = des.rearrange(sub_key, tables.extensionPermutationTable)
+        result = des.bit_arr_to_str(result)
+        self.assertEqual(result, '010000000101010011111011111010100111110101011001')
+
+    def test_xor_arr(self):
+        exp_block = des.text_to_arr('010000000101010011111011111010100111110101011001')
+        sub_key = des.text_to_arr('100011111110011010110000011111010011100100010000')
+
+        result = des.xor_arr(exp_block, sub_key)
+        result = des.bit_arr_to_str(result)
+
+        self.assertEqual(result, '110011111011001001001011100101110100010001001001')
+
+    def test_s_box(self):
+        self.assertEqual(des.get_s_box([0, 1, 1, 0, 1, 0], 0), [1, 0, 0, 1])
+        self.assertEqual(des.get_s_box([1, 1, 0, 0, 1, 0], 0), [1, 1, 0, 0])
+        self.assertEqual(des.get_s_box([1, 1, 0, 0, 1, 0], 0), [1, 1, 0, 0])
+        self.assertEqual(des.get_s_box([0, 0, 0, 0, 1, 1], 0), [1, 1, 1, 1])
+
+    def split_arr_8(self):
+        arr = des.text_to_arr('110011111011001001001011100101110100010001001001')
+        arr = des.split_arr_to_chunks(arr, 6)
+        self.assertEqual(len(arr), 8)
+        self.assertEqual(arr[0], [1, 1, 0, 0])
+        self.assertEqual(arr[1], [1, 1, 1, 1])
+        self.assertEqual(arr[-1], [1, 0, 0, 1])
+
+    def test_s_box_substitution(self):
+        xor = des.text_to_arr('110011111011001001001011100101110100010001001001')
+        desired = des.text_to_arr('10110101001111111100010011101010')
+        result = des.s_box_substitution(xor)
+        self.assertEqual(desired, result)
+
+    def test_p_box_substitution(self):
+        sbox_output = des.text_to_arr('10110101001111111100010011101010')
+        desired = des.text_to_arr('10001101110101100101011001011111')
+
+        result = des.p_box_substitution(sbox_output)
+        self.assertEqual(desired, result)
+
+    def first_round(self):
+        plain = des.text_to_arr('0010100111011000001000110011011100111100010101100100100111111011')
+        key = des.text_to_arr('1101000111010100101000110101110010101010000100011101010010101010')
+        result = des.encrypt(plain, key)
+        desired = des.text_to_arr('1000001010011101110100111010110001101111011011000110111010010010')
+
+        self.assertEqual(desired, result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
